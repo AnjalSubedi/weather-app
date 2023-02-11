a45d61f6dd48f682075f41863933b6df0d6c3e5f
Anjalsubedi <anjalsubedi170@gmail.com>
2023-02-11 12:00:30 +0545

    API call

--- src/components/Choosestate/index.js
+++ src/components/Choosestate/index.js
@@ -1,14 +1,48 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import cities from '../../data/np.json'
-
+import { UseWeatherAppContext } from '../../context/Context';
+import axios from 'axios';
 const ChooseStateComponents = ()=>{
     
+    const{state:{city},dispatch} = UseWeatherAppContext();
+    console.log('UseWeatherAppContext',UseWeatherAppContext());
+   
+    const handlechange=(e)=>{
+        const selectedcity = cities.filter((city)=>{
+           return city.city === e.target.value
+        }
 
+        )[0]
+        // console.log(selectedcity);
+        dispatch ({
+            type:'SET_CITY',
+            payload:selectedcity
+        })
+    }
+    const APIKEY = '546dae70c8bf153f5c79dd1abe35bbe1';
+    let lat = city && city.lat ? city.lat : '';
+    let long = city && city.lng ? city.lng : '';
+    let exclude = 'hourly,minutely';
+    const URL =  `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=${exclude}&units=metric&lang=tr&appid=${APIKEY}`
+    
+    const fetchData = ()=>{
+        axios(URL).then((data)=>{
+            console.log(data)
+        }
+
+        )
+    }
+     useEffect(()=>{
+        fetchData();
+        // eslint-disable-next-line
+     },[city]
+
+     )
 
     return (
         <>
             <div className='stateWrap'>
-                <select className='stateMenu' >
+                <select className='stateMenu' defaultValue={city} onChange={handlechange} >
                     {
                 cities && cities.length > 0  && cities.map((city)=>{
 
--- src/context/Context.js
+++ src/context/Context.js
@@ -10,22 +10,22 @@ const WeatherAPPProvider = ({children})=>{
 
     const [state, dispatch] = useReducer(WeatherReducer, {
         city:{
-            "city": "Delhi", 
-            "lat": "28.6600", 
-            "lng": "77.2300", 
-            "country": "India", 
-            "iso2": "IN", 
-            "admin_name": "Delhi", 
-            "capital": "admin", 
-            "population": "29617000", 
-            "population_proper": "16753235"
+            "city": "Bharatpur", 
+             "lat": "27.6833", 
+           "lng": "84.4333", 
+           "country": "Nepal", 
+          "iso2": "NP", 
+          "admin_name": "Nārāyanī", 
+         "capital": "", 
+         "population": "143836", 
+         "population_proper": "143836"
           },
           current:'',
           daily:''
     })
 
     
-    //const APIKEY = ''
+    
     return (
         <WeatherAPPContext.Provider value={{state, dispatch}} >
             {children}
