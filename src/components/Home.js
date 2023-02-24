c71176b402f25ccffad29a6557bd22351a02085b
Anjalsubedi <anjalsubedi170@gmail.com>
2023-02-24 21:36:55 +0545

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -25,16 +25,6 @@ export default HomeComponents;
 
 
 
-// 
-// 34480b98aa332da53123a0ac63a4ea9d weather api key
-// 34480b98aa332da53123a0ac63a4ea9d
-// https://home.openweathermap.org/api_keys
 
-
-
-// https://simplemaps.com/data/in-cities
-// https://openweathermap.org/
-// https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-// https://api.openweathermap.org/data/3.0/onecall?lat=20&lon=85&appid=34480b98aa332da53123a0ac63a4ea9d
 
 
